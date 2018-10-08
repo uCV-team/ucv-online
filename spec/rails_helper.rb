768a390203e9d594666e30b15127bf744c2866b7
Eduard <ed.munteanu99@gmail.com>
2018-10-08 17:54:03 +0200

    Add first test to check if set up correctly

---
+++ spec/rails_helper.rb
@@ -0,0 +1,18 @@
+ENV['RAILS_ENV'] ||= 'test'
+require 'spec_helper'
+require File.expand_path('../config/environment', __dir__)
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
+require 'selenium/webdriver'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
