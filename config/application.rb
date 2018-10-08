670ecf0f6fb6a56ee590d1b898d6dd664f000527
Eduard <ed.munteanu99@gmail.com>
2018-10-08 15:15:28 +0200

    first commit

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module Publicv
+  class Application < Rails::Application
+    config.load_defaults 5.2
+
+    config.time_zone = 'Zurich'
+    config.i18n.default_locale = :en
+  end
+end
