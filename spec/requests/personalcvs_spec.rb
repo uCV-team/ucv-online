a6954f0f27643f23eb4429b17c3854cc54c5c548
Eduard <ed.munteanu99@gmail.com>
2018-10-10 14:32:03 +0200

    Remove unnecessary tests, small fixes

--- spec/requests/personalcvs_spec.rb
+++ spec/requests/personalcvs_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Personalcvs', type: :request do
-  describe 'GET /personalcvs' do
-    it 'works! (now write some real specs)' do
-      get personalcvs_path
+  describe '#new' do
+    it 'returns a page' do
+      get new_personalcv_path
       expect(response).to have_http_status(:ok)
     end
   end
