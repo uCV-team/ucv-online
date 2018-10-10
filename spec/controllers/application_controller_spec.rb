e0ecd7271b7fb336f738cc2a038ec73e83330710
Eduard <ed.munteanu99@gmail.com>
2018-10-10 10:37:23 +0200

    Add request spec for registrations controller

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  describe '#configure_permitted_parameters'
+  it 'adds first_name to permitted parameters' do
+  end
+end
