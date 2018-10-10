f46b948f325c0354f643165a2d7056dbe62e9bc5
Eduard <ed.munteanu99@gmail.com>
2018-10-10 10:41:15 +0200

    Remove controller spec

--- spec/controllers/application_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationController, type: :controller do
-  describe '#configure_permitted_parameters'
-  it 'adds first_name to permitted parameters' do
-  end
-end
