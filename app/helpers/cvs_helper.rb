fe8a87714195a87c7e295e1d21a1f72abe256ed6
Eduard Munteanu <eduard.munteanu@renuo.ch>
2018-11-13 13:44:16 +0100

    Improve #study_period helper method

--- app/helpers/cvs_helper.rb
+++ app/helpers/cvs_helper.rb
@@ -4,6 +4,12 @@ module CvsHelper
   end
 
   def study_period(started_on, ended_on)
-    "#{started_on.year} - #{ended_on.year}" if started_on.present? && ended_on.present?
+    if started_on.present? && ended_on.present?
+      "#{started_on.year} - #{ended_on.year}" if started_on.present? && ended_on.present?
+    elsif started_on.present?
+      t('content.main.cv.show.education.on_going', started_on: started_on.year)
+    else
+      ''
+    end
   end
 end
