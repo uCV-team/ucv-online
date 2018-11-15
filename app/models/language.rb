a1312faf41ea8d9557ae971993842c3b62eeee1b
Eduard Munteanu <eduard.munteanu@renuo.ch>
2018-11-15 16:33:42 +0100

    Add level validation to languages

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,8 @@
 class Language < ApplicationRecord
   belongs_to :cv
 
+  CEFR_LEVELS = %w[a1 a2 b1 b2 c1 c2].freeze
+
   validates :language, presence: true
+  validates :level, inclusion: { in: CEFR_LEVELS }
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Language do
   it { is_expected.to validate_presence_of(:language) }
+  it { is_expected.to validate_inclusion_of(:level).in_array(Language::CEFR_LEVELS) }
 end
