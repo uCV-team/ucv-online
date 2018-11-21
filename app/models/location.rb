bc5396a33c1a6da78621840b068dfbf28e50434a
Eduard Munteanu <eduard.munteanu@renuo.ch>
2018-11-21 10:24:08 +0100

    Setup locationIQ + geocoder

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,10 @@ class Location < ApplicationRecord
   belongs_to :user
   scope :chronological_order, -> { order(created_at: :desc) }
 
+  geocoded_by :address
+
   validates :address, :title, presence: true
   validates :radius, numericality: { greater_than_or_equal_to: 0 }
+
+  after_validation :geocode, if: ->(obj) { obj.address.present? && obj.address_changed? }
 end
