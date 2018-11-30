464d0b62c1b1b78cfa015df9dcd8abec8782082f
Eduard Munteanu <eduard.munteanu@renuo.ch>
2018-11-30 13:51:10 +0100

    Fix turbolinks issue

--- app/webpacker/src/javascripts/google_maps.js
+++ app/webpacker/src/javascripts/google_maps.js
@@ -1,10 +1,21 @@
-function initMap() {
+let resultsList;
+
+window.initMap = function() {
     $('#map').each((_, mapElement) => {
+        console.log('loaded');
         new google.maps.Map(mapElement, {
             center: {lat: 46.8182, lng: 8.2275},
             zoom: 7
         });
     });
+};
+
+function setResultsList(formattedSearchResults) {
+
 }
 
-window.initMap = initMap;
+$(document).on('turbolinks:load', () => {
+    if ('google' in window) {
+        initMap();
+    }
+});
