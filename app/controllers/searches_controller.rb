f1e5699ef0b9037994e2e08448b0bfb6f4a0c5fe
Eduard Munteanu <eduard.munteanu@renuo.ch>
2018-11-30 16:35:07 +0100

    Implement marker generation

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,15 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @results = Cv.full_text_search(search_param)
+    if search_param.present?
+      @results = Cv.full_text_search(search_param)
+      @formatted_results = SearchesService.new(@results.first(10)).coordinates_list
+      respond_to do |format|
+        format.js { render partial: 'search_results' }
+      end
+    else
+      @results = []
+    end
   end
 
   private
--- app/webpacker/src/javascripts/google_maps.js
+++ app/webpacker/src/javascripts/google_maps.js
@@ -1,17 +1,33 @@
-let resultsList;
+let map;
+let markers = [];
 
 window.initMap = function() {
     $('#map').each((_, mapElement) => {
-        console.log('loaded');
-        new google.maps.Map(mapElement, {
+        map = new google.maps.Map(mapElement, {
             center: {lat: 46.8182, lng: 8.2275},
             zoom: 7
         });
     });
 };
 
-function setResultsList(formattedSearchResults) {
+window.generateMarkers = function(searchResultsList) {
+    clearMarkers();
+    searchResultsList.forEach(entry => {
+        markers.push(
+            new google.maps.Marker({
+                position: entry.location,
+                map: map,
+                title: entry.name
+            })
+        );
+    });
+};
 
+function clearMarkers() {
+    markers.forEach(marker => {
+       marker.setMap(null);
+    });
+    markers = [];
 }
 
 $(document).on('turbolinks:load', () => {
