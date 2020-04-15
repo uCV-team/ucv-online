af7e664d0a48002468b4688710a42d9d40c0e386
vishal <vishal@bluebash.co>
2020-04-15 09:50:26 +0530

    set i18 local

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name tel])
+  end
+
+  def set_locale
+    case tld
+    when 'it'
+      I18n.locale = :it
+    else
+      I18n.locale = :en
+    end
+  end
+
+  def tld
+    @tld ||= request.host.split('.').last
   end
 end
