9c6dbd7c57ff46cdb2edabc15ee4692961407292
vishal <vishal@bluebash.co>
2020-04-15 10:04:53 +0530

    updated default local

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module Publicv
     config.load_defaults 5.2
 
     config.time_zone = 'Rome'
-    config.i18n.default_locale = :it
+    config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.action_mailer.default_url_options = { host: ENV['APP_HOST'], port: ENV['APP_PORT'] }
   end
