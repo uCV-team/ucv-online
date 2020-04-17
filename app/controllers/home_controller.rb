8425ef6d757c159a58cebf5ffd7260721e618d73
Rahul <rahul@bluebash.co>
2020-04-17 19:09:19 +0530

    Integrate New design layout - home page
    - Update assets to latest version
    - Fix issue with fontawesome
    - Created default layout and cleanup old assets
    - Added images to the webpacker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,2 @@
 class HomeController < ApplicationController
-  layout 'theme', except: [:coming_soon]
-
-  def coming_soon
-    render 'coming_soon', layout: false
-  end
 end
--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,10 +1,11 @@
 import 'babel-polyfill';
-import 'bootstrap/dist/js/bootstrap';
+import '../src/javascripts/bootstrap';
 import '../src/javascripts/address_autocomplete';
 import '../src/javascripts/google_maps';
 import jQuery from 'jquery';
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
+require.context('../images', true)
 
 window.$ = jQuery;
 Rails.start();
