9bfaec69e68494c92493b91360fc21482e0345a6
Rahul <rahul@bluebash.co>
2020-04-25 16:16:35 +0530

    Fixed for the CV section and PDF download improvements
    - Fix for the icons in PDF
    - Content limitation for education and experience
    - Removed dummy content from top bar

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,7 +1,8 @@
 class Education < ApplicationRecord
+  DESCRIPTION_MAX_LENGTH = 255
   belongs_to :cv
-
   validates :school, presence: true
+  validates :description, length: { maximum: DESCRIPTION_MAX_LENGTH }
 
   scope :chronological_order, -> { order(created_at: :desc) }
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,7 +1,9 @@
 class Experience < ApplicationRecord
-  belongs_to :cv
+  DESCRIPTION_MAX_LENGTH = 255
 
+  belongs_to :cv
   validates :company, :title, presence: true
+  validates :description, length: { maximum: DESCRIPTION_MAX_LENGTH }
   validates :website_url, format: { with: %r{\A(http|https)://[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(/.*)?\z}ix,
                                     message: 'is invalid' }, allow_blank: true
 
