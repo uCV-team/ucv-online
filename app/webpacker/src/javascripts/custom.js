07cc976b59596c15d8aa420417eba284ebafbad1
Rahul <rahul@bluebash.co>
2020-04-30 22:56:47 +0530

    Add URL scheme for redirect

--- app/webpacker/src/javascripts/custom.js
+++ app/webpacker/src/javascripts/custom.js
@@ -2,14 +2,16 @@ let selectTag;
 
 function handleSelect(){
   selectTag = document.getElementsByClassName("language-switcher")[0];
-  if (window.currentLocale == 'en') selectTag.getElementsByTagName('option')[0].selected = 'selected';
-  else selectTag.getElementsByTagName('option')[1].selected = 'selected';
+  if (selectTag.length > 0) {
+    if (window.currentLocale == 'en') selectTag.getElementsByTagName('option')[0].selected = 'selected';
+    else selectTag.getElementsByTagName('option')[1].selected = 'selected';
 
-  selectTag.addEventListener('change', function(){
-    let arr = window.location.host.split('.');
-    arr[arr.length-1] = this.value;
-  	window.location = arr.join('.')
-  })
+    selectTag.addEventListener('change', function(){
+      let arr = window.location.host.split('.');
+      arr[arr.length-1] = this.value;
+      window.location = "http://"+arr.join('.')
+    })
+  }
 }
 
 $(document).on('turbolinks:load', () => {
