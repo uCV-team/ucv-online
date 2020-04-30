138bd85cc24a5b025b6fef35f7a06e04c3bce83b
Rahul <rahul@bluebash.co>
2020-04-30 21:57:48 +0530

    Added locale switcher

---
+++ app/webpacker/src/javascripts/custom.js
@@ -0,0 +1,17 @@
+let selectTag;
+
+function handleSelect(){
+  selectTag = document.getElementsByClassName("language-switcher")[0];
+  if (window.currentLocale == 'en') selectTag.getElementsByTagName('option')[0].selected = 'selected';
+  else selectTag.getElementsByTagName('option')[1].selected = 'selected';
+
+  selectTag.addEventListener('change', function(){
+    let arr = window.location.host.split('.');
+    arr[arr.length-1] = this.value;
+  	window.location = arr.join('.')
+  })
+}
+
+$(document).on('turbolinks:load', () => {
+  handleSelect();
+});
