e79d4283a6a3fd863e03b8fe7c6a3050d29b31c4
Kanchan <kanchan@bluebash.co>
2021-04-05 17:06:57 +0530

    Updated Application.js file in app/javascript directory #84

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,41 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+// console.log('Hello World from Webpacker')
+// import 'babel-polyfill';
+
+// require("jquery");
+
+// import Rails from 'rails-ujs';
+// import Turbolinks from 'turbolinks';
+// Rails.start();
+// Turbolinks.start();
+
+// require.context('../src/javascripts', true);
+// require.context('../images', true);
+// require.context('../icons', true);
+// import 'jquery-ui-dist/jquery-ui';
+
+// window.Rails = Rails
+
+
+import 'babel-polyfill';
+import 'bootstrap/dist/js/bootstrap';
+import '../src/javascripts/address_autocomplete';
+import '../src/javascripts/locationiq_maps';
+import '../src/javascripts/custom';
+import jQuery from 'jquery';
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+require.context('../images', true)
+require.context('../icons', true)
+
+window.$ = jQuery;
+Rails.start();
+Turbolinks.start();
+
+import 'jquery-ui-dist/jquery-ui';
+
+$(function () {
+  console.log('Hello World from Webpacker');
+});
