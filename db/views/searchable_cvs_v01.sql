9640bf1342a6030d604ddd60e5f614ae6347f5ea
vishal <vishal@bluebash.co>
2021-05-10 02:12:43 +0530

    Updated materialized views

--- db/views/searchable_cvs_v01.sql
+++ db/views/searchable_cvs_v01.sql
@@ -1,12 +1,19 @@
 SELECT
  cvs.id as cv_id,
  users.id as user_id,
+ users.subdomain as subdomain,
+ users.first_name as first_name,
+ users.last_name as last_name,
+ MIN(locations.latitude) as latitude,
+ MIN(locations.longitude) as longitude,
+ MIN(locations.original_address) as original_address,
 
  (
     setweight(to_tsvector('cv_en', coalesce(cvs.about , '')), 'B') ||
     setweight(to_tsvector('cv_en', coalesce(cvs.skills , '')), 'B') ||
     setweight(to_tsvector('cv_en', coalesce(cvs.working_skills , '')), 'A') ||
-    setweight(to_tsvector('cv_en', coalesce(STRING_AGG(users.first_name || users.last_name, ' '), '')), 'A') ||
+    setweight(to_tsvector('cv_en', coalesce(users.first_name , '')), 'A') ||
+    setweight(to_tsvector('cv_en', coalesce(users.last_name , '')), 'A') ||
     setweight(to_tsvector('cv_en', coalesce(STRING_AGG(locations.city || locations.country, ' '), '')), 'B') ||
     setweight(to_tsvector('cv_en', coalesce(STRING_AGG(locations.geocoded_address || locations.original_address, ' '), '')), 'D') ||
     setweight(to_tsvector('cv_en', coalesce(STRING_AGG(educations.degree || educations.description || educations.school, ' '), '')), 'D') ||
@@ -18,7 +25,8 @@ SELECT
     setweight(to_tsvector('cv_it', coalesce(cvs.about , '')), 'B') ||
     setweight(to_tsvector('cv_it', coalesce(cvs.skills , '')), 'B') ||
     setweight(to_tsvector('cv_it', coalesce(cvs.working_skills , '')), 'A') ||
-    setweight(to_tsvector('cv_it', coalesce(STRING_AGG(users.first_name || users.last_name, ' '), '')), 'A') ||
+    setweight(to_tsvector('cv_it', coalesce(users.first_name , '')), 'A') ||
+    setweight(to_tsvector('cv_it', coalesce(users.last_name , '')), 'A') ||
     setweight(to_tsvector('cv_it', coalesce(STRING_AGG(locations.city || locations.country, ' '), '')), 'B') ||
     setweight(to_tsvector('cv_it', coalesce(STRING_AGG(locations.geocoded_address || locations.original_address, ' '), '')), 'D') ||
     setweight(to_tsvector('cv_it', coalesce(STRING_AGG(educations.degree || educations.description || educations.school, ' '), '')), 'D') ||
@@ -29,10 +37,10 @@ SELECT
 FROM cvs
 
 INNER JOIN users ON users.id = cvs.user_id
-INNER JOIN locations ON locations.user_id = users.id
-INNER JOIN educations ON educations.cv_id = cvs.id
-INNER JOIN languages ON languages.cv_id = cvs.id
-INNER JOIN experiences ON experiences.cv_id = cvs.id
+LEFT JOIN locations ON locations.user_id = users.id
+LEFT JOIN educations ON educations.cv_id = cvs.id
+LEFT JOIN languages ON languages.cv_id = cvs.id
+LEFT JOIN experiences ON experiences.cv_id = cvs.id
 
 WHERE 
   cvs.published = true
