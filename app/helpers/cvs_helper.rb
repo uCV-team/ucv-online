8e83df31db10c1367000c5cf08560c4d327cb015
Darmenise <duccioarmenise@gmail.com>
2021-06-29 11:55:03 +0300

    naming and copy amended

--- app/helpers/cvs_helper.rb
+++ app/helpers/cvs_helper.rb
@@ -25,9 +25,9 @@ module CvsHelper
     cv_obj.publish_last_name? ? cv_obj.full_name : cv_obj.abbr_name
   end
 
-  def info_icon(title)
+  def tooltip_icon(title)
     content_tag(:div, class: 'tool-tip text-muted mr-2', data: { toggle: 'tooltip', placement: 'left' },
-                      title: t("tooltips.info.#{title}")) do
+                      title: t("tooltips.#{title}")) do
       content_tag(:i, '', class: 'info-icon fas fa-info-circle')
     end
   end
