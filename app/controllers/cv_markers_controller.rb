a4446fd94960281474ddd95ea7fa63cccec4fdc7
Kanchan <kanchan@bluebash.co>
2021-08-17 19:30:57 +0530

    displayed published cvs on map

--- app/controllers/cv_markers_controller.rb
+++ app/controllers/cv_markers_controller.rb
@@ -2,8 +2,11 @@ class CvMarkersController < ApplicationController
   def index
     bound_params = JSON.parse(params[:bounds])
     top, left, bottom, right = bound_params
-    results_markers = Location.includes(:cv).where('latitude > ? AND latitude < ? AND longitude > ? AND longitude < ?',
-                                                   bottom.to_f, top.to_f, left.to_f, right.to_f)
+    results_markers = Location
+                      .includes(cv: [:user])
+                      .joins(:cv)
+                      .where('latitude > ? AND latitude < ? AND longitude > ? AND longitude < ? AND cvs.published = ?',
+                             bottom.to_f, top.to_f, left.to_f, right.to_f, true)
     @formatted_results = SearchesService.new(results_markers).coordinates_list
     render json: @formatted_results
   end
