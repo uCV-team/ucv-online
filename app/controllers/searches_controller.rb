90199450e9586d35f2f0c479867b01d917889e21
Kanchan <kanchan@bluebash.co>
2021-08-21 11:58:30 +0530

    fixed SERP map centering

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,4 @@
 class SearchesController < ApplicationController
-  include InitialMapCentering
-  before_action :set_initial_map_position
-
   def create
     if search_param.present?
       @search = Search.find_or_create_by(query: sanitized_query, locale: I18n.locale.to_s)
@@ -16,10 +13,11 @@ class SearchesController < ApplicationController
     @search = Search.friendly.find(params[:id])
     seo_tags_for(@search)
     @search.increment!(:views)
-    all_results = @search.materialized_views_results
-    @total_results = all_results.count
-    @results = all_results.page(params[:page]).per(10)
-    @formatted_results = SearchesService.new(all_results.includes(cv: [:user])).coordinates_list
+    @all_results = @search.materialized_views_results
+    @total_results = @all_results.count
+    @results = @all_results.page(params[:page]).per(10)
+    @formatted_results = SearchesService.new(@all_results.includes(cv: [:user])).coordinates_list
+    search_map_center
     respond_to do |format|
       format.html
       format.js { render partial: 'search_results' }
@@ -34,5 +32,18 @@ class SearchesController < ApplicationController
 
   def sanitized_query
     Search.sanitized_query(search_param)
+  end
+
+  def search_map_center
+    final_results = @all_results.select(&:latitude)
+    if final_results.any?
+      lat = final_results.first.latitude
+      lng = final_results.first.longitude
+      @coordinates = [lng, lat]
+      @set_zoom = 2.5
+    else
+      @coordinates = [20, 50]
+      @set_zoom = 0.9
+    end
   end
 end
