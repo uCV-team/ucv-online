7f273b66d719b5092e511a79caeb5e52b723cb74
Kanchan <kanchan@bluebash.co>
2021-09-06 19:16:50 +0530

    restricted user to manage their records #208

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,18 +4,18 @@ class Ability
   def initialize(current_user)
     guest_permissions
     return unless current_user
-    user_permissions
+    user_permissions(current_user)
   end
 
   def guest_permissions
     can :read, Location
-    can :read, Cv
+    can :read, Cv, published: true
     can :create, Contact
   end
 
-  def user_permissions
-    can :manage, User
-    can :manage, Contact
+  def user_permissions(current_user)
+    can :manage, User, id: current_user.id
+    can :manage, Contact, user: current_user
     can :manage, Cv
     can :manage, Education
     can :manage, Experience
