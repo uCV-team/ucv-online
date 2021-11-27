4874d1ef75ecee62acb70556f65d94cfb9a9171e
Ritik <ritik.sharma@bluebash.co>
2021-11-27 11:49:08 +0530

    PR feedback updates and add cache to features CV's

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
 
   def index
     @seo_title = I18n.t('home.seo_title')
-    @featured_cvs = Cv.most_recent
+    @featured_cvs = Cv.featured_last_4
     @featured_searches = Search.ordered.localized.limit(15)
     @cvs_last_updated_count = Cv.where('updated_at > ?', 30.days.ago).count
   end
