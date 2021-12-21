52c96c72261028fc2e6e1fd49892f02ee261488e
Ritik <ritik.sharma@bluebash.co>
2021-12-21 18:40:54 +0530

    Updated map message position to top

--- app/javascript/src/javascripts/homepage_map.js
+++ app/javascript/src/javascripts/homepage_map.js
@@ -186,15 +186,20 @@ window.initIndexMap = function () {
 };
 
 function scrollMap() {
-  map.on("wheel", event => {
-    if (event.originalEvent.ctrlKey) {
-      document.getElementById('scroll').classList.add('d-none')
-      return;
-    } else {
-      document.getElementById('scroll').classList.remove('d-none')
-    }
-    event.preventDefault();
-  });
+  if ($(window).width() > 767) {
+    map.on("wheel", event => {
+      if (event.originalEvent.ctrlKey) {
+        document.getElementById('scroll').classList.remove('map-message')
+        return;
+      } else {
+        document.getElementById('scroll').classList.add('map-message')
+      }
+      event.preventDefault();
+      setTimeout(function(){
+        document.getElementById('scroll').classList.remove('map-message')
+      }, 1500);
+    });
+  }
 }
 
 function multiTouchSupport() {
