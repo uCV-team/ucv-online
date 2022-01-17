1348f231283fb95de2848c5ecc5c73470baa304d
Ritik <ritik.sharma@bluebash.co>
2022-01-17 17:34:24 +0530

    Fixes for sending mails based on locale

--- app/mailers/passwordless/mailer.rb
+++ app/mailers/passwordless/mailer.rb
@@ -7,6 +7,7 @@ module Passwordless
     # Sends a magic link (secret token) email.
     # @param session [Session] A Passwordless Session
     def magic_link(session)
+      @current_locale = session.authenticatable.locale
       @session = session
 
       @magic_link = send(Passwordless.mounted_as)
@@ -21,6 +22,7 @@ module Passwordless
     end
 
     def user_confirmation_instructions(recipient, token)
+      @current_locale = recipient.locale
       @email = recipient.unconfirmed_email.presence || recipient.email
       @magic_link = send(Passwordless.mounted_as)
                     .token_sign_in_url(token)
